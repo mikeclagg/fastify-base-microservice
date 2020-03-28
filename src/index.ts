54cdf65cd0185095c2f47e80a361e462d202feae
Mike C <mikeclagg@gmail.com>
2020-03-28 04:03:35 -0500

    skeleton only

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
 import { BaseMicroService } from './server';
-import { consoleError } from './helpers/utils';
 
-import { SERVER_PORT, HTTP_PROTOCOL, HTTP_HOSTNAME } from './constants';
-import { ServiceConfig } from './interfaces';
-import { routes } from './routes';
-
-const cfg: ServiceConfig = {
-  port: SERVER_PORT,
-  protocol: HTTP_PROTOCOL,
-  hostname: HTTP_HOSTNAME
-};
-
-const server = new BaseMicroService(cfg);
-server.init(routes);
-server.start()
-      .then(() => console.log(`Server - ${cfg.protocol}://${cfg.hostname}:${cfg.port}`))
-      .catch(consoleError);
+export default BaseMicroService;
